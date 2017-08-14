8dce705b24dc44eae9fdeb87255c0e920ffb1025
Dan Nollette and safiy cham <safiy@>
2017-08-14 10:11:31 -0700

    refactor code to DRY it out.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,10 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var animalInput = $("input#animal").val();
-    var exclamationInput = $("input#exclamation").val();
-    var verbInput = $("input#verb").val();
-    var nounInput = $("input#noun").val();
-
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
-
+    var words = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
+    words.forEach(function(word) {
+      var wordText = $("input#" + word).val();
+      $("." + word).text(wordText);
+    });
     $("#story").show();
     event.preventDefault();
   });
